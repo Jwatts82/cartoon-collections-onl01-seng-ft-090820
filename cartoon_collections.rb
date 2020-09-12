3a4aa5faff5ecfad64fea34ada5aa30e9853cefc
Jessica Watts <jessperr182@hotmail.com>
2020-09-12 20:42:02 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,7 +20,9 @@ def long_planeteer_calls(long_planeteer_calls)
    answer
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(cheese)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
+  cheese_types[2]
 end
