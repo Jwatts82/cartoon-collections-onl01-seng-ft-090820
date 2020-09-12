b4d7fe803c1aa78c15986078a3062d54e9ce10c3
Jessica Watts <jessperr182@hotmail.com>
2020-09-12 20:32:52 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,8 @@ end
 
 def long_planeteer_calls(long_planeteer_calls)
   answer = false
-  
+  long_planeteer_calls.each do |call|
+    if #{call}
 end
 
 def find_the_cheese# code an argument here
